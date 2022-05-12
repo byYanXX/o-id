305fe9f869388799c13a2171ad98d0807fd3decc
chusight <80103970+chusight@users.noreply.github.com>
2022-05-12 19:20:28 +0800

    Update GitHubShareButton.tsx

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://vercel.com/chusight/o-id'
+      href='https://github.com/chusight/o-id'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
